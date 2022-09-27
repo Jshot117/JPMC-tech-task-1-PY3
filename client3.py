a16884819be412052094649d7ad9603ea38e4be9
Jonnathan Saavedra <jonnjstart@gmail.com>
2022-09-27 16:30:35 -0400

    I added interator to only execute once in the for loop. Saving the price of a stock on the varibale firstPrice

--- client3.py
+++ client3.py
@@ -35,14 +35,15 @@ def getDataPoint(quote):
 	stock = quote['stock']
 	bid_price = float(quote['top_bid']['price'])
 	ask_price = float(quote['top_ask']['price'])
-	price = bid_price
+	price = (bid_price + ask_price)/2
 	return stock, bid_price, ask_price, price
 
 def getRatio(price_a, price_b):
 	""" Get ratio of price_a and price_b """
 	""" ------------- Update this function ------------- """
 	""" Also create some unit tests for this function in client_test.py """
-	return 1
+	ratio = price_a / price_b
+	return ratio
 
 # Main
 if __name__ == "__main__":
@@ -52,8 +53,12 @@ if __name__ == "__main__":
 		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
 		""" ----------- Update to get the ratio --------------- """
+		iterator = 0
 		for quote in quotes:
 			stock, bid_price, ask_price, price = getDataPoint(quote)
+			if ( iterator == 0 ):
+				firstPrice = price
 			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+			iterator= iterator + 1
 
-		print ("Ratio %s" % getRatio(price, price))
+		print ("Ratio %s" % getRatio(firstPrice, price))
